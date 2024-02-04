73a55299ddc679d3771ad2edde595c113fcd8801
Hyungjoong-Park <yhcauserie@naver.com>
2024-02-04 19:09:47 +0900

    map apply

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
 
 import React, {Component } from 'react';
 import logo from './logo.svg';
-import Customer from './components/BsnCustomer'
+import BsnCustomer from './components/BsnCustomer'
 import './App.css';
 
-const customer = {
-  'name': '홍길동',
-  'birthday': '710426',
-  'gender': '남자',
-  'job': '대학생'
+const bsncust = [{
+  'seq': 1,
+  'image': 'https://picsum.photos/64/64/?image=1',
+  'bsnno': '1050718143',
+  'bsnname': '웰메이드닷컴',
+  'bsnperson': '박형중',
+  'telno': '01032111236',
+  'bsnpost': '05791',
+  'bsnbasicaddr': '서울특별시 송파구 위례송파로 40',
+  'bsndetailaddr': '1206동 101호',
+  'bsnstatecd': '1:정상',
+  'tradesalecnt': 10,
+  'tradebuycnt': 2,
+  'outsourcingsalecnt': 5,
+  'outsourcingbuycnt': 3,
+  'delyn': 'N',
+  'regdate': '2021-08-30 22:51:12 123',
+  'uptdate': '2021-08-30 22:51:12 123'
+},
+{
+  'seq': 2,
+  'image': 'https://picsum.photos/64/64/?image=2',
+  'bsnno': '1050718143',
+  'bsnname': '웰메이드닷컴',
+  'bsnperson': '박형중',
+  'telno': '01032111236',
+  'bsnpost': '05791',
+  'bsnbasicaddr': '서울특별시 송파구 위례송파로 40',
+  'bsndetailaddr': '1206동 101호',
+  'bsnstatecd': '1:정상',
+  'tradesalecnt': 10,
+  'tradebuycnt': 2,
+  'outsourcingsalecnt': 5,
+  'outsourcingbuycnt': 3,
+  'delyn': 'N',
+  'regdate': '2021-08-30 22:51:12 123',
+  'uptdate': '2021-08-30 22:51:12 123'
+},
+{
+  'seq': 3,
+  'image': 'https://picsum.photos/64/64/?image=3',
+  'bsnno': '1050718143',
+  'bsnname': '웰메이드닷컴',
+  'bsnperson': '박형중',
+  'telno': '01032111236',
+  'bsnpost': '05791',
+  'bsnbasicaddr': '서울특별시 송파구 위례송파로 40',
+  'bsndetailaddr': '1206동 101호',
+  'bsnstatecd': '1:정상',
+  'tradesalecnt': 10,
+  'tradebuycnt': 2,
+  'outsourcingsalecnt': 5,
+  'outsourcingbuycnt': 3,
+  'delyn': 'N',
+  'regdate': '2021-08-30 22:51:12 123',
+  'uptdate': '2021-08-30 22:51:12 123'
 }
-
+]
 class App extends Component {
   render () {
     return (
-          <Customer
-          name={customer.name}
-          birthday={customer.birthday}
-          gender={customer.gender}
-          job={customer.job}
-          />
+      <div>
+          {
+            bsncust.map(c => {
+              return (
+                <BsnCustomer
+                  seq={c.seq}
+                  image={c.image}
+                  bsnno={c.bsnno}
+                  bsnname={c.bsnname}
+                  bsnperson={c.bsnperson}
+                  telno={c.telno}
+                  bsnpost={c.bsnpost}
+                  bsnbasicaddr={c.bsnbasicaddr}
+                  bsndetailaddr={c.bsndetailaddr}
+                  bsnstatecd={c.bsnstatecd}
+                  tradesalecnt={c.tradesalecnt}
+                  tradebuycnt={c.tradebuycnt}
+                  outsourcingsalecnt={c.outsourcingsalecnt}
+                  outsourcingbuycnt={c.outsourcingbuycnt}
+                  delyn={c.delyn}
+                  regdate={c.regdate}
+                  uptdate={c.uptdate}
+                />
+              );
+            })
+          }
+      </div>
     );
   }
 }
--- src/components/BsnCustomer.js
+++ src/components/BsnCustomer.js
@@ -1,16 +1,29 @@
 import React from 'react'
 
-class Customer extends React.Component {
+class BsnCustomer extends React.Component {
     render() {
         return (
             <div>
-                <h2>{this.props.name}</h2>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
+                <h2>seq={this.props.seq}</h2>
+                <img src={this.props.image} alt="profile" /> 
+                <p>bsnno={this.props.bsnno}</p>
+                <p>bsnname={this.props.bsnname}</p>
+                <p>bsnperson={this.props.bsnperson}</p>
+                <p>telno={this.props.telno}</p>
+                <p>bsnstatecd={this.props.bsnstatecd}</p>
+                <p>tradesalecnt={this.props.tradesalecnt}</p>
+                <p>tradebuycnt={this.props.tradebuycnt}</p>
+                <p>outsourcingsalecnt={this.props.outsourcingsalecnt}</p>
+                <p>outsourcingbuycnt={this.props.outsourcingbuycnt}</p>
+                <p>bsnpost={this.props.bsnpost}</p>
+                <p>bsnbasicaddr={this.props.bsnbasicaddr}</p>
+                <p>bsndetailaddr={this.props.bsndetailaddr}</p>
+                <p>delyn={this.props.delyn}</p>
+                <p>regdate={this.props.regdate}</p>
+                <p>uptdate={this.props.uptdate}</p>
             </div>
         )
     }
 }
 
-export default Customer;
+export default BsnCustomer;
